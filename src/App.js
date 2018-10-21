8b811bee6b9d69b18caf98eabe7e94a6a6bdde8a
andrewShillito <andrew.shillito88@gmail.com>
2018-10-21 18:31:03 +0000

    finished re-structuring

--- src/App.js
+++ src/App.js
@@ -34,32 +34,68 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <h2>Shopping List</h2>
-        <form onSubmit={this.addItem}>
-          <input
-            type="text"
-            placeholder="Enter New Item"
-            value={this.state.value}
-            onChange={this.handleChange}
+        <Header />    
+        <AppTitle />
+        <Form 
+          addItem={this.addItem}
+          value={this.state.value}
+          handleChange={this.handleChange}
+          inputIsEmpty={this.inputIsEmpty}
           />
-          <button disabled={this.inputIsEmpty()}>Add</button>
-        </form>
-
-        <button onClick={this.deleteLastItem} disabled={this.noItemsFound()}>
-          Delete Last Item
-        </button>
-
-        <p className="items">Items</p>
-        <ol className="item-list">
-          {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
-        </ol>
+        <DeleteButton 
+          deleteLastItem={this.deleteLastItem}
+          noItemsFound={this.noItemsFound}
+          />
+        <ListTitle />
+        <ItemList 
+          items={this.state.items}
+          />        
       </div>
     );
   }
+}
+
+function Header(props){
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">ReactND - Coding Practice</h1>
+    </header>
+  )
+}
+
+const AppTitle = () => (<h2>Shopping List</h2>);
+
+function Form(props){
+  return(
+    <form onSubmit={props.addItem}>
+      <input
+        type="text"
+        placeholder="Enter New Item"
+        value={props.value}
+        onChange={props.handleChange}
+        />
+      <button disabled={props.inputIsEmpty()}>Add</button>
+    </form>
+  )
+}
+
+function DeleteButton(props) {
+  return (
+    <button onClick={props.deleteLastItem} disabled={props.noItemsFound()}>
+      Delete Last Item
+    </button>
+  )
+}
+
+const ListTitle = () => (<p className="items">Items</p>);
+
+function ItemList(props) {
+  return (
+    <ol className="item-list">
+      {props.items.map((item, index) => <li key={index}>{item}</li>)}
+    </ol>
+  )
 }
 
 export default App;
